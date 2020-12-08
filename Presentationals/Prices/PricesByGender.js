6bbc0cdd9944f0b0a077245fb8d41c6492455079
mbknu <bouchelot.m@gmail.com>
2020-12-08 17:50:47 +0100

    display price, and style it

--- Presentationals/Prices/PricesByGender.js
+++ Presentationals/Prices/PricesByGender.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 
+import styles from "./pricesByGender.module.css";
+
 const PricesByGender = ({ women, men }) => {
   const [menHairCut, setMenHairCut] = useState(false);
   const [womenHairCut, setWomenHairCut] = useState(false);
@@ -8,88 +10,95 @@ const PricesByGender = ({ women, men }) => {
 
   const showWomenPrices = women[0].map((cut) => {
     return (
-      <>
-        <p>{cut.text}</p>
-        <p>{cut.price} €</p>
-      </>
+      <p className={styles.text_information}>
+        {cut.text} <p className={styles.price_information}>{cut.price} € </p>
+      </p>
     );
   });
   const showWomenForfeit = women[1].map((cut) => {
     return (
-      <>
-        <p>{cut.forfait_text}</p>
-        <p>{cut.price} €</p>
-      </>
+      <p className={styles.text_information}>
+        {cut.forfait_text}{" "}
+        <p className={styles.price_information}>{cut.price} € </p>
+      </p>
     );
   });
   const showWomenForfeitUnder20 = women[2].map((cut) => {
     return (
-      <>
-        <p>{cut.forfait_under20years_text}</p>
-        <p>{cut.price} €</p>
-      </>
+      <p className={styles.text_information}>
+        {cut.forfait_under20years_text}{" "}
+        <p className={styles.price_information}>{cut.price} € </p>
+      </p>
     );
   });
 
   const showMenPrices = men[0].map((cut) => {
     return (
-      <>
-        <p>{cut.text}</p>
-        <p>{cut.price} €</p>
-      </>
+      <p className={styles.text_information}>
+        {cut.text} <p className={styles.price_information}>{cut.price} € </p>
+      </p>
     );
   });
   const showMenForfeit = men[1].map((cut) => {
     return (
-      <>
-        <p>{cut.forfait_text}</p>
-        <p>{cut.price} €</p>
-      </>
+      <p className={styles.text_information}>
+        {cut.forfait_text}{" "}
+        <p className={styles.price_information}>{cut.price} € </p>
+      </p>
     );
   });
   const showMenForfeitUnder20 = men[2].map((cut) => {
     return (
-      <>
-        <p>{cut.forfait_under20years_text}</p>
-        <p>{cut.price} €</p>
-      </>
+      <p className={styles.text_information}>
+        {cut.forfait_under20years_text}{" "}
+        <p className={styles.price_information}>{cut.price} € </p>
+      </p>
     );
   });
 
   return (
     <div>
-      <button
-        onClick={() => {
-          setMenHairCut(true);
-          setWomenHairCut(false);
-        }}
-      >
-        Hommes
-      </button>
-      <button
-        onClick={() => {
-          setMenHairCut(false);
-          setWomenHairCut(true);
-        }}
-      >
-        Femmes
-      </button>
+      <div classname={styles.select_gender}>
+        <a
+          className={styles.gender}
+          onClick={() => {
+            setMenHairCut(true);
+            setWomenHairCut(false);
+          }}
+        >
+          Hommes
+        </a>
+        <a
+          className={styles.gender}
+          onClick={() => {
+            setMenHairCut(false);
+            setWomenHairCut(true);
+          }}
+        >
+          Femmes
+        </a>
+      </div>
+
       <div>
         {womenHairCut && (
-          <>
-            <div>{showWomenPrices}</div>
-            <div>FORFAIT : {showWomenForfeit}</div>
-            <div>- 20 ans : {showWomenForfeitUnder20}</div>
-          </>
+          <div className={styles.wrapper}>
+            <div className={styles.container}>{showWomenPrices}</div>
+            <div className={styles.container}>FORFAIT : {showWomenForfeit}</div>
+            <div className={styles.container}>
+              - 20 ans : {showWomenForfeitUnder20}
+            </div>
+          </div>
         )}
       </div>
-      <div>
+      <div className={styles.wrapper}>
         {menHairCut && (
-          <>
-            <div>{showMenPrices}</div>
-            <div>FORFAIT : {showMenForfeit}</div>
-            <div>- 20 ans : {showMenForfeitUnder20}</div>
-          </>
+          <div className={styles.wrapper}>
+            <div className={styles.container}>{showMenPrices}</div>
+            <div className={styles.container}>FORFAIT : {showMenForfeit}</div>
+            <div className={styles.container}>
+              - 20 ans : {showMenForfeitUnder20}
+            </div>
+          </div>
         )}
       </div>
     </div>
